23538f626722a6110f77426699b723ac62713a32
AurelieNexus <aurelie.cauvin@groupm.com>
2024-12-19 14:55:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ with tab1:
             variant="contained",
             start_icon=mt.icons.exit_to_app,
             onclick="none",
-            style={"color": "#4c8bf5", "background": "#FF4B4B"},
+            style={"color": "#4c8bf5", "background": "#4c8bf5"},
             href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id="
             + clientId
             + "&redirect_uri="
